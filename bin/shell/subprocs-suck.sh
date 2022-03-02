cfc4bf8366810dc9dfcc7249a117df362c483443
SimonWoodtli <xnasero@posteo.net>
2022-03-02 00:41:18 -0500

    refactor: delete empty lines

--- bin/shell/subprocs-suck.sh
+++ bin/shell/subprocs-suck.sh
@@ -1,7 +1,7 @@
 
-############################# SUBPROCS SUCK #############################
+############################# SUBPROCS SUCK ############################
 
-perg () {
+perg() {
   declare exp="$1"; shift
   if [[ -z "$*" ]]; then
     while IFS= read -r line; do
@@ -15,35 +15,26 @@ perg () {
     done < "$file"
   done
 }
-
-lower () {
+lower() {
   echo ${1,,}
 }
-
-upper () {
+upper() {
   echo ${1^^}
 }
-
-linecount () {
+linecount() {
    IFS=$'\n'
    declare a=($1)
    echo ${#a[@]}
 }
-
 # These are so much faster than basename and dirname.
-
-basepart () {
+basepart() {
   echo ${1##*/}
 }
-
-dirpart () {
+dirpart() {
   echo ${1%/*}
 }
-
-
 # A friendlier cat that does not invoke a subshell.
-
-kat () {
+kat() {
 	declare line
   if [[ -z "$*" ]]; then
     while IFS= read -r line; do
@@ -57,11 +48,9 @@ kat () {
     done < "$file"
   done
 }
-
 # Same as kat but skips blank and lines with optional whitespace that
 # begin with a comment character (#).
-
-katlines () {
+katlines() {
   for file in $*; do
     while IFS= read -r line; do
       [[ $line =~ ^\ *(#|$) ]] && continue
@@ -69,25 +58,21 @@ katlines () {
     done < "$file"
   done
 }
-
 # Moves an existing thing to the same path and name but with
 # ".preserved.<tstamp>" at the end and echoes the new location. Usually
 # preferable to destroying what was previously there. Can be used to roll
 # back changes transactionally.
-
-preserve () {
+preserve() {
   declare target="${1%/}"
   [[ ! -e "$target" ]] && return 1
   declare new="$target.preserved.$(tstamp)"
   mv "$target" "$new"
   echo "$new"
 }
-
 # Lists all the preserved files by matching the .preserved<tstamp> suffix.
 # If passed an argument limits to only those preserved files matching that
 # name (prefix).
-
-lspreserved () {
+lspreserved() {
   declare -a files
   if [[ -z "$1" ]]; then
       files=(*.preserved.[2-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
@@ -98,34 +83,28 @@ lspreserved () {
     echo "$i"
   done
 }
-
-lastpreserved () {
+lastpreserved() {
   mapfile -t  a < <(lspreserved "$1")
   declare n=${#a[@]}
   echo "${a[n-1]}"
 }
-
-rmpreserved () {
+rmpreserved() {
   while IFS= read -r line; do
     rm -rf "$line"
   done < <(lspreserved)
 }
-
 # Undos the last preserve performed on the given target.
-
-unpreserve () {
+unpreserve() {
   declare last=$(lastpreserved "$*")
   [[ -z "$last" ]] && return 1
   mv "$last" "${last%.preserved*}"
 }
-
-trash () {
+trash() {
   [[ -z "$TRASH" ]] && return 1
   mkdir -p "$TRASH"
   mv "$*" "$TRASH"
 }
-
-stringscan () {
+stringscan() {
   declare buf="$1"
   declare handle=(echo)
   [[ -n "$2" ]] && handle=($2)
@@ -133,24 +112,20 @@ stringscan () {
     ($handle ${buf:$i:1})
   done
 }
-
-firstline () {
+firstline() {
   declare line
   declare IFS=
   read -r line <"$1"
   echo $line
 }
-
-lastline () {
+lastline() {
   # Tail is fastest because it does a seek to the end of file.
   tail -1 "$1"
 }
-
-lastcmdline () {
+lastcmdline() {
   lastline "$HOME/.bash_history"
 }
-
-lastcmd () {
+lastcmd() {
   declare cl=$(lastcmdline)
   echo ${cl%% *}
 }
