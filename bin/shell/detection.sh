bd636d18ae47c36c3c95fee94e55fefec5575e15
SimonWoodtli <xnasero@posteo.net>
2022-03-02 00:58:10 -0500

    refactor: delete functions

--- bin/shell/detection.sh
+++ bin/shell/detection.sh
@@ -1,4 +1,5 @@
-############################# DETECTION #################################
+
+############################### DETECTION ##############################
 
 ## Detection of interactivity. Shell *must* be
 ## interactive or *none* of this applies.
@@ -17,38 +18,3 @@ case "$OS" in
   FreeBSD|Darwin) export PLATFORM=mac ;;
   *)              export PLATFORM=unknown ;;
 esac
-
-onmac () {
-  [ "$PLATFORM" = mac ] && return 0
-  return 1
-}
-
-onwin () {
-  [ "$PLATFORM" == windows ]  && return 0
-  return 1
-}
-
-onlinux () {
-  [ "$PLATFORM" == linux ]  && return 0
-  return 1
-}
-
-onunknown () {
-  [ "$PLATFORM" == unknown ]  && return 0
-  return 1
-}
-
-havecmd () {
-  type "$1" &> /dev/null
-  return $?
-}
-
-for dir in repos downloads pictures; do
-  upper=${dir^^}
-  cap=${dir^}
-  if [ -d "$HOME/$cap" ]; then
-    eval "export $upper='$HOME/$cap'"
-  elif [ -d "$HOME/$dir" ]; then
-    eval "export $upper='$HOME/$dir'"
-  fi
-done
